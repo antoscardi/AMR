5146801e4362a947ac59be0a1642ff6d778d0cfa
antoscardi <antonio.scard97@gmail.com>
2023-04-14 16:29:28 +0200

    pulizia totate

--- my_functions/robot_system.m
+++
@@ -1,9 +0,0 @@
-function currentState = robot_system( oldInput, oldState, delta, params)
-%% Function that given the input and current state of the robot calculates the derivative of the state
-%  and integrates q_dot = f(q,u) to obtain the next State vector q [x,y,theta] at  the succesive time step k+1
-
-[~, qint] = ode45(@(t,q) q_dot(t,q,oldInput,params),[0 delta],oldState);
-currentState = qint(end,:)';
-
-%currentState = oldState + delta*q_dot(oldState,oldInput,params);
-end
